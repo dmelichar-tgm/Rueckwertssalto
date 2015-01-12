fff287cf62edb8cfc4a2aa8ee543f849b90e2a57
Tharnawn <danielmelichar0905@gmail.com>
2015-01-12 19:16:22 +0100

    test

---
+++ src/at/tgm/insy/backflip/prototype/Test.java
@@ -0,0 +1,13 @@
+package at.tgm.insy.backflip.prototype;
+
+/**
+ * @author Daniel Melichar
+ * @version 12.01.2015
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
