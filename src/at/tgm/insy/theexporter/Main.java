20a18805fc3338afd492272ecfc360caca65d351
Tharnawn <danielmelichar0905@gmail.com>
2015-01-12 19:15:03 +0100

    Initial commit

---
+++ src/at/tgm/insy/theexporter/Main.java
@@ -0,0 +1,19 @@
+package at.tgm.insy.theexporter;
+
+import at.tgm.insy.theexporter.cli.CommandLineController;
+import at.tgm.insy.theexporter.output.OutputController;
+
+/**
+ * Main class of the application
+ */
+public class Main {
+
+    /**
+     * Main Method of the Application.
+     * Calls upon the Output Controller with a CommandLineController 
+     * @param args Console arguments
+     */
+    public static void main(String[] args) {
+        new OutputController(new CommandLineController(args));
+    }
+}
