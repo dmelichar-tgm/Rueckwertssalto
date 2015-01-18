7046b24b8f38e05ce2876345c21f3946dc9698f3
Tharnawn <danielmelichar0905@gmail.com>
2015-01-18 14:17:55 +0100

    Updated doc

---
+++ src/at/tgm/insy/theexporter/test/CommandLineControllerTest.java
@@ -0,0 +1,68 @@
+package at.tgm.insy.theexporter.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandLineControllerTest {
+
+    @Test
+    public void testGetHost() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPassword() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDatabase() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDatabaseType() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOrderBy() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOrderSort() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFilter() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSeparator() throws Exception {
+
+    }
+
+    @Test
+    public void testGetExpression() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOutputFile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTable() throws Exception {
+
+    }
+}
